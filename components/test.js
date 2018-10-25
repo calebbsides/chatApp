21ecd37eeed828d3359ee34ca3255f2c91f87355
Caleb Sides <caleb@Calebs-MacBook-Pro.local>
2018-10-25 19:19:08 -0400

    Initial Commit

---
+++ components/test.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import { Text } from "react-native";
+
+export default class Test extends React.Component {
+    render () {
+        return <Text>Hello, World</Text>
+    }
+}
---
+++ constants/APIKeys.js
@@ -0,0 +1,10 @@
+export default {
+    FirebaseConfig: {
+        apiKey: "AIzaSyDVWSo8m1Jetq5pbLlkWgUu-ZLT1pvqVIE",
+        authDomain: "chat-app-fac88.firebaseapp.com",
+        databaseURL: "https://chat-app-fac88.firebaseio.com",
+        projectId: "chat-app-fac88",
+        storageBucket: "chat-app-fac88.appspot.com",
+        messagingSenderId: "229949904368"
+    },
+}

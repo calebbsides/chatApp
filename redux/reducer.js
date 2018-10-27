92af3839a8c22909d28c500522774cf13114fa03
Caleb Sides <caleb@Calebs-MacBook-Pro.local>
2018-10-27 15:43:36 -0400

    App is now working with new heroku server

--- redux/reducer.js
+++ redux/reducer.js
@@ -2,7 +2,7 @@ import Pusher from 'pusher-js/react-native';
 
 const initialState = {
     pusher: new Pusher('34b5eb49f328341df2f1', {
-        authEndpoint: 'https://nuggetchat.herokuapp.com/',
+        authEndpoint: 'https://nuggetchat.herokuapp.com/pusher/auth',
         cluster: 'us2',
         encrypted: true
     }),
